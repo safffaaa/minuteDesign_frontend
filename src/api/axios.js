fcd0ad898efacaa2e5e21dd0d784c006aaec223a
safa saleem o <safasaleem916@gmail.com>
2025-10-30 17:27:56 +0530

    second commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:7000/api/",
+  baseURL: "https://minutedesign-backend.onrender.com/api/",
 });
 
 export default API;
